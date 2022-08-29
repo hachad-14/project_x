7dddbffa52ceb5f1e836cd68e8db799406edd811
Legion <hachadsaad14@gmail.com>
2022-08-30 00:55:11 +0100

    test

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -40,16 +40,17 @@ class _HomePageState extends State<HomePage> {
         _speechToText.listen(
             localeId: _currentLocaleId,
             onResult: (result) => setState(() {
-                  fText = result.recognizedWords;
-                  translate();
-                }));
+              fText = result.recognizedWords;
+              translate();
+            }
+          )
+        );
       }
     } else {
       setState(() {
         islistening = false;
       });
       _speechToText.stop();
-      islistening = false;
     }
   }
 
@@ -75,7 +76,7 @@ class _HomePageState extends State<HomePage> {
 
   void speak() async {
     speaker.setVolume(1.0);
-    speaker.setRate(1.0);
+    speaker.setRate(0.7);
     speaker.setPitch(1.0);
     speaker.setLanguage("es");
     speaker.speak(rText);
@@ -90,164 +91,131 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(children: <Widget>[
-          Align(
-            alignment: Alignment(0, 0),
-            child: Container(
-              margin: const EdgeInsets.only(top: 30),
-              height: 60,
-              width: 380,
-              decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        spreadRadius: 0.5,
-                        blurRadius: 2,
-                        offset: Offset(0, 1))
-                  ]),
+      body: ListView(
+        physics: const NeverScrollableScrollPhysics(),
+        children: <Widget>[
+        Align(
+          alignment: Alignment(0, 0),
+          child: Container(
+            margin: const EdgeInsets.only(top: 30),
+            height: 60,
+            width: 380,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow:[BoxShadow(color: Colors.black.withOpacity(0.3),spreadRadius: 0.5,blurRadius: 2,offset: Offset(0, 1))]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(right: 120, left: 120),
+                  child: Text("Project X v0.2",style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold)),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment(0, 0),
+          child: Container(
+            margin: const EdgeInsets.only(top: 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Container(
+                    width: 380,
+                    height: 250,
+                    decoration: BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0),borderRadius: BorderRadius.circular(20),boxShadow: [
+                    BoxShadow(color: Colors.black.withOpacity(0.1),spreadRadius: 0.5,blurRadius: 2,offset: Offset(0, 1))]),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 22, right: 22),
+                          child: Text(fText,textAlign: TextAlign.center,style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Container(
+                    width: 380,
+                    height: 170,
+                    decoration: BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0),borderRadius: BorderRadius.circular(20),boxShadow: [
+                    BoxShadow(color: Colors.black.withOpacity(0.1),spreadRadius: 0.5,blurRadius: 2,offset: Offset(0, 1))]),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(rText,textAlign: TextAlign.center,style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 42),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 130, left: 0),
+                        child: FloatingActionButton(
+                          onPressed: () {
+                            copy();
+                            print("copy");
+                          },
+                          child: Icon(islistening? Icons.copy: Icons.copy_sharp),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(0),
+                        child: FloatingActionButton(
+                          onPressed: () {
+                            speak();
+                            print("speak");
+                          },
+                          child: Icon(islistening? Icons.speaker: Icons.speaker),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ]
+            )
+          ),
+        ),
+      ]),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: islistening,
+        glowColor: Colors.red,
+        endRadius: 80,
+        duration: Duration(seconds: 1),
+        repeat: true,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.only(right: 120, left: 120),
-                    child: Text("Project X v0.2",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold)),
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      listen();
+                      print("object");
+                    },
+                    child: Icon(islistening ? Icons.mic : Icons.mic_none),
                   ),
                 ],
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment(0, 0),
-            child: Container(
-                margin: const EdgeInsets.only(top: 0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(top: 20),
-                        child: Container(
-                          width: 380,
-                          height: 250,
-                          decoration: BoxDecoration(
-                              color: Color.fromRGBO(0, 0, 0, 0),
-                              borderRadius: BorderRadius.circular(20),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Colors.black.withOpacity(0.1),
-                                    spreadRadius: 0.5,
-                                    blurRadius: 2,
-                                    offset: Offset(0, 1))
-                              ]),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(left: 22, right: 22),
-                                child: Text(fText,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.bold)),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 20),
-                        child: Container(
-                          width: 380,
-                          height: 170,
-                          decoration: BoxDecoration(
-                              color: Color.fromRGBO(0, 0, 0, 0),
-                              borderRadius: BorderRadius.circular(20),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Colors.black.withOpacity(0.1),
-                                    spreadRadius: 0.5,
-                                    blurRadius: 2,
-                                    offset: Offset(0, 1))
-                              ]),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(rText,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold)),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 45),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(right: 130, left: 0),
-                              child: FloatingActionButton(
-                                onPressed: () {
-                                  copy();
-                                  print("copy");
-                                },
-                                child: Icon(islistening
-                                    ? Icons.copy
-                                    : Icons.copy_sharp),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.all(0),
-                              child: FloatingActionButton(
-                                onPressed: () {
-                                  speak();
-                                  print("speak");
-                                },
-                                child: Icon(islistening
-                                    ? Icons.speaker
-                                    : Icons.speaker),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ])),
-          ),
-        ]),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: AvatarGlow(
-          animate: islistening,
-          glowColor: Colors.red,
-          endRadius: 80,
-          duration: Duration(seconds: 1),
-          repeat: true,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FloatingActionButton(
-                      onPressed: () {
-                        listen();
-                        print("object");
-                      },
-                      child: Icon(islistening ? Icons.mic : Icons.mic_none),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ));
+          ],
+        ),
+      )
+    );
   }
 }
